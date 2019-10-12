858a441d28f168b7f1a5bc12381823ea10b14df1
Jagoda Bodnar <jagodabodnar90@gmail.com>
2019-10-12 22:44:18 +0200

    final

--- js/script.js
+++ js/script.js
@@ -20,8 +20,8 @@ document.querySelector('.a-navigation-sidebar_hamburger').addEventListener('clic
 function toggleWallet(){
         document.querySelector('.a-navigation-mainmenu_itemDropdownContent').classList.toggle('active');
         document.querySelector('.a-navigation-mainmenu_itemWallet').classList.toggle('active');  
-    }
-document.querySelector('#a-arrow-down').addEventListener('click', function(e) {
+    };
+document.querySelector('#down').addEventListener('click', function(e) {
         e.preventDefault();
         toggleWallet();
         change();
@@ -29,9 +29,9 @@ document.querySelector('#a-arrow-down').addEventListener('click', function(e) {
 
 
 function change() {
-            var img1 = "https://cdn3.iconfinder.com/data/icons/faticons/32/arrow-up-01-512.png",
+            let img1 = "https://cdn3.iconfinder.com/data/icons/faticons/32/arrow-up-01-512.png",
                 img2 = "https://cdn3.iconfinder.com/data/icons/faticons/32/arrow-down-01-512.png";
-            var imgElement = document.getElementById('a-arrow-down');
+            let imgElement = document.getElementById('a-arrow-down');
             imgElement.src = (imgElement.src === img1)? img2 : img1;
          }
 
@@ -41,9 +41,9 @@ document.querySelector('.a-navigation-sidebar_itemDropdown').addEventListener('c
             });
 
 function changeSideBar() {
-            var img1 = "https://cdn3.iconfinder.com/data/icons/faticons/32/arrow-up-01-512.png",
+            let img1 = "https://cdn3.iconfinder.com/data/icons/faticons/32/arrow-up-01-512.png",
                 img2 = "https://cdn3.iconfinder.com/data/icons/faticons/32/arrow-down-01-512.png";
-            var imgElement = document.getElementById('a-arrow-down-sidebar');
+            let imgElement = document.getElementById('a-arrow-down-sidebar');
             imgElement.src = (imgElement.src === img1)? img2 : img1;
          }
 
@@ -52,7 +52,8 @@ var dropdown = document.getElementsByClassName("a-navigation-sidebar_itemDropdow
 var i;
 
 for (i = 0; i < dropdown.length; i++) {
-  dropdown[i].addEventListener("click", function() {
+  dropdown[i].addEventListener("click", function(e) {
+    e.preventDefault();
   var dropdownContent = document.getElementById('a-navigation-sidebar_itemDropdownContent');
   dropdownContent.style.display === "block" ? dropdownContent.style.display = "none" : dropdownContent.style.display = "block";
   });
@@ -63,14 +64,16 @@ var dropdown = document.getElementsByClassName("notification");
 var i;
 
 for (i = 0; i < dropdown.length; i++) {
-  dropdown[i].addEventListener("click", function() {
+  dropdown[i].addEventListener("click", function(e) {
+    e.preventDefault();
   var dropdownContent = document.getElementById('a-navigation-sidebar_messagesDropdown');
   dropdownContent.style.display === "block" ? dropdownContent.style.display = "none" : dropdownContent.style.display = "block";
   document.querySelector('.a-navigation-sidebar_notification').classList.toggle('active'); 
   });
 }
 
-function notification(){
+function notification(e){
+    e.preventDefault();
     document.querySelector('.a-navigation-mainmenu_itemNotifications').classList.toggle('disabled');
     document.querySelector('.a-navigation-mainmenu_itemProfile').classList.toggle('disabled');
     document.querySelector('.a-navigation-mainmenu_itemNotificationMessages').classList.toggle('active'); 
@@ -79,48 +82,52 @@ function notification(){
 document.querySelector('.a-navigation-mainmenu_itemNotifications').addEventListener('click', notification);
 document.querySelector('#notification-exit').addEventListener('click', notification);
 
-/*CHART*/
-var ctx = document.getElementById('myChart').getContext('2d');
-var chart = new Chart(ctx, {
-    // 1
-    type: 'bar',
-    data: {
-        // 2
-        labels: ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10"],
-        // 3
-        datasets: [{
-            // 4
-            label: "Signups",
-            // 5
-            backgroundColor: '#8DBEC8',
-            borderColor: '#8DBEC8',
-            // 6
-            data: [ 52, 51, 41, 94, 26, 6, 72, 9, 21, 88 ],
-        },
-        {
-            label: "FTD",
-            backgroundColor: '#F29E4E',
-            borderColor: '#F29E4E',
-            data: [ 6, 72, 1, 0, 47, 11, 50, 44, 63, 76 ],
-        },
-        {
-            label: "Earned",
-            backgroundColor: '#71B374',
-            borderColor: '#71B374',
-            data: [ 59, 49, 68, 90, 67, 41, 13, 38, 48, 48 ],
-            // 7
-            hidden: true,
-        }]
-    },
-    options: {
-        legend: {
-            display: true,
-            labels: {
-                usePointStyle: true,
-                fontFamily: 'Roboto',
-                fontSize: 15,
-            }
-        },
+
+/*MODAL 1*/
+
+function closeModal() {
+    document.getElementById('overlay').classList.remove('show')
+  };
+
+document.querySelectorAll('#overlay .js--close-modal').forEach(function(btn) {
+    btn.addEventListener('click', function(e) {
+      e.preventDefault()
+      closeModal()
+    })
+  });
+  document.querySelector('#overlay').addEventListener('click', function(e) {
+    if(e.target === this) {
+      closeModal()
     }
+  });
+  document.addEventListener('keyup', function(e) {
+    if(e.keyCode === 27) {
+      closeModal()
+    }
+  });
+  
+  function openModal(modal) {
+    document.querySelectorAll('#overlay > *').forEach(function(modal){
+        modal.classList.remove('show');
+    })
+    document.querySelector('#overlay').classList.add('show');
+    document.querySelector(modal).classList.add('show');
+  };
+  
+  
+  document.getElementById('a-navigation-mainmenu_itemProfile').addEventListener('click', function(){
+        openModal('#myModal');
+      });
     
-});
+  document.getElementById('exit-button').addEventListener('click', function(){
+        openModal('#myModalTwo');
+      });
+  document.getElementById('exit').addEventListener('click', function(){
+        openModal('#myModalTwo');
+      });
+
+  document.getElementById('notification').addEventListener('click', function(){
+        openModal('#myModal');
+      });
+
+      
